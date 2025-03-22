18d87018398e3b1a410b0994ae9e6668a4ad692c
usov <spb-public@yandex.ru>
2025-03-22 23:40:17 +0300

    firs commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <map>
+#include <iostream>
+#include "myspace.h"
+
+//
+int main()
+{
+    std::cout << "Home work #3" << std::endl;
+
+    using Element = int;
+    static const int ItemsCount {10};
+
+    {
+        std::map<Element, Element> std_map_std_allc;
+        // std map std allocator
+        for (auto i = 0;i < ItemsCount;++i)
+            std_map_std_allc.insert({i, my::factorial<Element>(i)});
+        std::cout << "std map std allocator [size: " << std_map_std_allc.size() << "]> ";
+        for (const auto &item: std_map_std_allc)
+            std::cout << "{" << item.first << ": " << item.second << "} ";
+        std::cout << std::endl;
+    }
+
+    {
+        // std map custom allocator
+        std::map<Element, Element, std::less<Element>, my::allocator<ItemsCount, Element>> std_map_cstm_allc;
+        for (auto i = 0;i < ItemsCount;++i)
+            std_map_cstm_allc.insert({i, my::factorial<Element>(i)});
+        std::cout << "std map custom allocator [size: " << std_map_cstm_allc.size() << "]> ";
+        for (const auto &item: std_map_cstm_allc)
+            std::cout << "{" << item.first << ": " << item.second << "} ";
+        std::cout << std::endl;
+    }
+
+    {
+        // custom container
+        my::container<Element> cstm_cntr_std_allc;
+        for (auto i = 0;i < ItemsCount;++i)
+            cstm_cntr_std_allc.insert(i);
+        std::cout << "custom container std allocator [size: " << cstm_cntr_std_allc.size() << "]> ";
+        for (const auto &item: cstm_cntr_std_allc)
+            std::cout << "{" << item << "} ";
+        std::cout << std::endl;
+    }
+
+    {
+        // custom container custom allocator
+        my::container<Element, my::allocator<ItemsCount, Element>> cstm_cntr_cstm_allc;
+        for (auto i = 0;i < ItemsCount;++i)
+            cstm_cntr_cstm_allc.insert(i);
+        std::cout << "custom container custom allocator [size: " << cstm_cntr_cstm_allc.size() << "]> ";
+        for (const auto &item: cstm_cntr_cstm_allc)
+            std::cout << "{" << item << "} ";
+        std::cout << std::endl;
+    }
+
+    {
+        // custom container list init custom allocator
+        my::container<Element, my::allocator<ItemsCount, Element>> cstm_cntr_cstm_allc_list {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::cout << "custom container list init custom allocator [size: " << cstm_cntr_cstm_allc_list.size() << "]> ";
+        for (const auto &item: cstm_cntr_cstm_allc_list)
+            std::cout << "{" << item << "} ";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ myspace.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+
+namespace my {
+
+// calc factorial
+template<typename T>
+T factorial(T n)
+{
+    return (n > 1) ? (n * factorial<T>(n - 1)) : 1;
+}
+
+template<typename T, typename Alloc = std::allocator<T>>
+class container
+{
+public:
+    container()
+    {
+    }
+
+    container(std::initializer_list<T> list)
+    {
+        for (const auto &item: list)
+            insert(item);
+    }
+
+    ~container()
+    {
+        m_allc.deallocate(m_ptr, m_size);
+        m_ptr = nullptr;
+    }
+
+    void insert(const T &value)
+    {
+        if ((m_size + 1) > m_cap) {
+            m_cap = m_size + 1;
+
+            const auto ptr = m_allc.allocate(m_cap);
+            for (size_t i = 0;i < m_size;++i)
+                ptr[i] = m_ptr[i];
+
+            m_allc.deallocate(m_ptr, m_size);
+            m_ptr = ptr;
+        }
+        m_ptr[m_size++] = value;
+    }
+
+    const T *begin() const
+    {
+        return m_ptr;
+    }
+
+    const T *end() const
+    {
+        return m_ptr + m_size;
+    }
+
+    size_t size() const
+    {
+        return m_size;
+    }
+
+    bool empty() const
+    {
+        return (m_size == 0);
+    }
+
+private:
+    size_t m_cap {0};
+    Alloc m_allc;
+    T *m_ptr {nullptr};
+    size_t m_size {0};
+
+};
+
+template<const int size, typename T>
+class allocator : public std::allocator<T>
+{
+public:
+
+    // T *allocate(std::size_t n)
+    // {
+    //     std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
+    //
+    //     auto p = std::malloc(n * sizeof(T));
+    //     if (!p)
+    //         throw std::bad_alloc();
+    //     return reinterpret_cast<T *>(p);
+    // }
+
+};
+
+}
+
